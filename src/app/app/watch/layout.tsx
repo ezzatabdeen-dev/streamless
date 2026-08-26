374ed91a6efd9d17215170501dd3cce5325b2e88
Ezzat Abdeen <ezzatabdeen.dev@gmail.com>
2026-08-26 15:49:43 +0300

    Delete src/app/app/watch directory

--- src/app/app/watch/layout.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/app/watch/layout.tsx
-export default function WatchLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="-mx-0 lg:mx-0">{children}</div>;
-}
